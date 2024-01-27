7e7e3ea6fe058baa98997514e819aff8ffb0c7fe
razku <senkaotaku@gmail.com>
2024-01-27 02:18:39 +0100

    Local multiplayer functionality

--- ggj2024/Assets/Scripts/CarInputHandler.cs
+++ ggj2024/Assets/Scripts/CarInputHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CarInputController : MonoBehaviour
 {
+    public int playerNumber = 1;
     //Components
     CarController carController;
 
@@ -17,8 +18,18 @@ public class CarInputController : MonoBehaviour
     {
         Vector2 inputVector = Vector2.zero;
 
-        inputVector.x = Input.GetAxis("Horizontal");
-        inputVector.y = Input.GetAxis("Vertical");
+        switch (playerNumber) 
+        {
+            case 1:
+                inputVector.x = Input.GetAxis("Horizontal_P1");
+                inputVector.y = Input.GetAxis("Vertical_P1");
+                break;
+
+            case 2:
+                inputVector.x = Input.GetAxis("Horizontal_P2");
+                inputVector.y = Input.GetAxis("Vertical_P2");
+                break;
+        }
 
         carController.SetInputVector(inputVector);
     }
