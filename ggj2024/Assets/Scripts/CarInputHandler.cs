17a2b08b14ad8d31614786f4889deaa564bac44f
Gito66 <106806526+Gito66@users.noreply.github.com>
2024-01-28 03:20:35 +0100

    semaforo

--- ggj2024/Assets/Scripts/CarInputHandler.cs
+++ ggj2024/Assets/Scripts/CarInputHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CarInputController : MonoBehaviour
 {
+    public float tiempoEspera = 3.5f;
+    float ticker;
     public int playerNumber = 1;
     public int multx = 1;
     public int multy = 1;
@@ -18,19 +20,22 @@ public class CarInputController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ticker += Time.deltaTime;
+
         Vector2 inputVector = Vector2.zero;
+         if (ticker >= tiempoEspera) {
+            switch (playerNumber)
+            {
+                case 1:
+                    inputVector.x = Input.GetAxis("Horizontal_P1") * multx;
+                    inputVector.y = Input.GetAxis("Vertical_P1") * multy; ;
+                    break;
 
-        switch (playerNumber)
-        {
-            case 1:
-                inputVector.x = Input.GetAxis("Horizontal_P1") * multx;
-                inputVector.y = Input.GetAxis("Vertical_P1") * multy; ;
-                break;
-
-            case 2:
-                inputVector.x = Input.GetAxis("Horizontal_P2") * multx;
-                inputVector.y = Input.GetAxis("Vertical_P2") * multy;
-                break;
+                case 2:
+                    inputVector.x = Input.GetAxis("Horizontal_P2") * multx;
+                    inputVector.y = Input.GetAxis("Vertical_P2") * multy;
+                    break;
+            }
         }
 
         carController.SetInputVector(inputVector);
